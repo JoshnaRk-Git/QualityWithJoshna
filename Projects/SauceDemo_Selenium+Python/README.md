d6a030a0e4fb837f37f7dec7dd442fefdba383cc
Joshna D'Costa <joshna.dcosta@gmail.com>
2025-07-24 11:29:32 +0530

    Initial commit with SauceDemo selenium project

---
+++ Projects/SauceDemo_Selenium+Python/README.md
@@ -0,0 +1,30 @@
+**# SauceDemo Selenium + Pytest Automation**
+
+
+
+This project automates login functionality testing for \[SauceDemo](https://www.saucedemo.com/) using Selenium WebDriver and Pytest.
+
+
+
+**## Folder Structure**
+
+
+
+\- `test\_scripts/` - All test scripts
+
+\- `screenshots/` - Captured screenshots (optional)
+
+\- `Jenkinsfile` - CI configuration for running tests in Jenkins
+
+
+
+**## Tools Used**
+
+
+
+\- Python
+
+\- Selenium
+
+\- Pytest
+
---
+++ Projects/SauceDemo_Selenium+Python/test_scripts/Test/TestLogin.py
@@ -0,0 +1,45 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../..')))
+
+from Pages.TestloginPage import loginPage
+from Test.TestData import ValidPassword, ValidUsername, InvalidUsername, URL
+
+import time
+import pytest
+from selenium import webdriver
+#from test_scripts.Pages.TestloginPage import loginPage
+#from test_scripts.Test.TestData import ValidPassword, ValidUsername, InvalidUsername, URL
+
+@pytest.fixture
+def driver():
+    driver= webdriver.Chrome()
+    yield driver
+    driver.quit()
+
+@pytest.fixture
+def openUrl(driver):
+    lPage=loginPage(driver)
+    lPage.open(URL)
+    time.sleep(1)
+    return lPage
+
+# Test Case 1: Valid Login
+def test_validCredentials(openUrl):
+    openUrl.enterUsername(ValidUsername)
+    openUrl.enterPassword(ValidPassword)
+    openUrl.clickLogin()
+    time.sleep(2)
+    assert openUrl.driver.current_url.endswith("/inventory.html")
+
+# Test Case 2: Invalid Login
+def test_invalidCredentials(openUrl):
+    openUrl.enterUsername(InvalidUsername)
+    openUrl.enterPassword(ValidPassword)
+    openUrl.clickLogin()
+    Error=openUrl.getError()
+    time.sleep(1)
+    assert Error is not None
+
+
+
